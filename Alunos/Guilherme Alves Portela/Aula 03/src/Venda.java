5d814c78aef6d67de8067c86555fdabdd870aa3a
Guilherme A. Portela <guilherme.portela4004@gmail.com>
2026-03-14 16:04:30 -0300

    Update Venda.java
    Venda object updates

--- Alunos/Guilherme Alves Portela/Aula 03/src/Venda.java
+++ Alunos/Guilherme Alves Portela/Aula 03/src/Venda.java
@@ -4,10 +4,30 @@ public class Venda {
     public double desconto; 
     
     public Venda(int quantidade, double valor, double desconto){
+        if (valor <= 0) {
+            System.out.println("Valor da flor deve ser maior que zero.");
+            return;
+        }
+        if (quantidade <= 0) {
+           System.out.println("Quantidade de flores não pode ser menor ou igual a zero.");
+           return;
+        }
+
         this.quantidade = quantidade;
         this.valor = valor;
         this.desconto = desconto;
     }
 
+    //Getters
+    public int getQuantidade() { return quantidade; }
+    public double getValor() { return valor;}
+    public double getDesconto(){ return desconto}
 
+    public String toString() {
+        return "Valor: R$ " + String.format("%.2f", valor) + " | Quantidade: " + quantidade + " | Desconto: " + String.format("%.2f", desconto);
+    }
+    
+    //Setters
+
+    
 }
