db6b23144c2746fc26da4b1238fa7e6a47f01db4
joaopedromorenoo <joaopdromoreno@gmail.com>
2026-03-14 23:37:55 -0300

    Realizado Aula 01,02 e 03

--- Alunos/Joao Pedro Moreno/Aula 02/Main.java
+++ Alunos/Joao Pedro Moreno/Aula 02/Main.java
@@ -1,12 +1,12 @@
     import java.util.Scanner;
+	import java.util.ArrayList;
 
 public class Main {
 
+	static ArrayList<Planta> historico = new ArrayList<>();
 	public static Scanner scan = new Scanner(System.in);
 	static double totalCompra;
-
 	public static void main(String[] args) {
-		
 		VerMenu();
 	}
 
@@ -19,15 +19,17 @@ public class Main {
 			System.out.println("|--------------Menu---------------|");
 			System.out.println("|1- Calcular Preço Total          |");
 			System.out.println("|2- Calcular troco                |");
-			System.out.println("|3- Sair                          |");
+			System.out.println("|3- Histórico de Vendas           |");
+			System.out.println("|4- Sair                          |");
 			System.out.println("|Escolha uma opção:               |");
 			System.out.printf("|_________________________________|\n");
 			escolha = scan.nextInt();
 			scan.nextLine();
 			validarEscolha(escolha);
-		} while (escolha != 3);
+		} while (escolha != 4);
 	}
 
+	//leitura da escolha feita e direcionamento
 	public static void validarEscolha(int escolha) {
 		switch (escolha) {
 		case 1: {
@@ -39,6 +41,10 @@ public class Main {
 			break;
 		}
 		case 3: {
+			HistoricoVendas();
+			break;
+		}
+		case 4: {
 			System.out.printf("Você saiu do sistema! Obrigado, volte sempre!");
 			return;
 		}
@@ -47,32 +53,55 @@ public class Main {
 		}
 	}
 
+	//cálculo do Preço total
 	public static void CalcularPrecoTotal() {
 
 		Planta p = new Planta();
 
-		p.leituraQuant();
-    	p.leituraValor();
+		p.LeituraNome();
+		p.LeituraQuant();
+    	p.LeituraValor();
 
-    totalCompra = p.calcularPrecoTotal();
+   		totalCompra = p.calcularPrecoTotal();
 
-    System.out.println("O valor total das plantas: " + totalCompra);
+		historico.add(p);//guarda a venda
 
+    	System.out.println("O valor total das plantas: " + totalCompra);
 	}
 
+	//cálculo do Troco 
 	public static void CalcularTroco() {
 
-     System.out.println("\nInforme o valor pago pelo cliente:");
-    double valorPago = scan.nextDouble();
+    	System.out.println("\nInforme o valor pago pelo cliente:");
+    	double valorPago = scan.nextDouble();
 
-    if (valorPago < totalCompra) {
-        System.out.println("Valor insuficiente! O cliente deve pagar pelo menos: " + totalCompra + " reais");
-    } else {
+    	if (valorPago < totalCompra) {
+        	System.out.println("Valor insuficiente! O cliente deve pagar pelo menos: " + totalCompra + " reais");
+   		} else {
 
         double troco = valorPago - totalCompra;
 
         System.out.println("Total da compra: " + totalCompra);
         System.out.println("Troco do cliente: " + troco);
-    }
-}
+    	}
+	}
+
+	//ArrayList das Vendas
+	public static void HistoricoVendas(){
+		
+		if (historico.isEmpty()) {
+        System.out.println("Nenhuma venda registrada.");
+    	} else {
+
+        	System.out.println("\n___________________________________");
+        	System.out.println("|-------HISTÓRICO DE VENDAS-------|");
+
+        	for (Planta p : historico) {
+            System.out.println(p);
+        	}
+
+        	System.out.println("|                                 |");
+			System.out.println("|_________________________________|");
+		}
+	}
 }
--- Alunos/Joao Pedro Moreno/Aula 02/Planta.java
+++ Alunos/Joao Pedro Moreno/Aula 02/Planta.java
@@ -4,21 +4,29 @@ public class Planta {
 
     static Scanner scan = new Scanner(System.in);
 
+    String nome;
     int quant;
     double valor;
+    double total;
 
     public Planta() {
     }
 
-    public int leituraQuant() {
+    public String LeituraNome(){
+        System.out.println("\nInforme a espécie da planta:");
+        nome = scan.nextLine();
+        return nome;      
+    }
 
-        System.out.println("\nInforme a quantidade da planta:");
+    public int LeituraQuant() {
+
+        System.out.println("Informe a quantidade da planta:");
         quant = scan.nextInt();
         scan.nextLine();
         return quant;
     }
 
-    public double leituraValor() {
+    public double LeituraValor() {
 
         System.out.println("Informe o valor da planta:");
         valor = scan.nextDouble();
@@ -28,14 +36,18 @@ public class Planta {
 
     public double calcularPrecoTotal() {
         
-        double total = quant * valor;
+        total = quant * valor;
 
-            if (quant > 10) {
+            if (quant >= 10) {
                 double desconto = total * 0.05;
                 total = total - desconto;
-                System.out.println("Desconto de 5% aplicado!");
+                System.out.println("Desconto de 5% foi aplicado!");
             }
     
         return total;
+    }
+
+     public String toString() {
+        return "|Planta: " + nome + "  Valor: " + total;
     }
 }
